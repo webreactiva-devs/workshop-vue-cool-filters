da7212666ffa89ad43fa7103163b61b0f6c46c35
Daniel Primo <delineas@gmail.com>
2022-06-21 22:05:13 +0200

    step-06 Remove method and attr

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <button @click="changeImage">Cambiar imagen</button>
-  <input type="color" @input="changeBorderColor($event)" :value="borderColor" />
+  <input type="color" v-model="borderColor" />
   <hr />
   <img :src="imgSrc" :style="`border: 20px solid ${borderColor}`" />
 </template>
@@ -18,9 +18,6 @@ export default {
       return (this.imgSrc = `https://source.unsplash.com/200x200/?cocktail,party&v=${Math.floor(
         Math.random() * 300
       )}`);
-    },
-    changeBorderColor($event) {
-      this.borderColor = $event.target.value;
     }
   },
 };
