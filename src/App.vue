1ce5f88dfa120910427b94e64f68aa2fe0b3cfd5
Daniel Primo <delineas@gmail.com>
2022-06-29 09:10:25 +0200

    step-09 Add Grayscale filter

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
+  <img :src="imgSrc" :style="imgStyle" />
+  <hr />
   <button @click="changeImage">Cambiar imagen</button>
   <input type="color" v-model="borderColor" />
   <hr />
-  <img :src="imgSrc" :style="imgStyle" />
+  <label for="grayscaleFilter">Escala de gris</label>
+  <input type="range" v-model="grayscaleFilter" min="0" max="100" step="1" />
+  <hr />
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { ref, computed } from "vue";
 
 const borderColor = ref("#FF0000");
-const imgSrc = ref("https://source.unsplash.com/200x200/?cocktail,party&v=1");
+const grayscaleFilter = ref(0);
+const imgSrc = ref("https://source.unsplash.com/400x400/?cocktail,party&v=1");
 
 const changeImage = () => {
-  imgSrc.value = `https://source.unsplash.com/200x200/?cocktail,party&v=${Math.floor(
+  imgSrc.value = `https://source.unsplash.com/400x400/?cocktail,party&v=${Math.floor(
     Math.random() * 300
   )}`;
 };
@@ -22,11 +27,8 @@ const imgStyle = computed(() => {
     "border-width": "20px",
     borderStyle: "solid",
     borderColor: borderColor.value,
+    filter: `grayscale(${grayscaleFilter.value}%)`,
   };
 });
-
-onMounted(() => {
-  document.querySelector('img').style.borderColor = 'blue'
-})
 
 </script>
