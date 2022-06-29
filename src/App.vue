88e8cedb9ce6a34121ad436180a98e24d661aa8a
Daniel Primo <delineas@gmail.com>
2022-06-29 09:14:21 +0200

    step-10 Change ref to reactive

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <img :src="imgSrc" :style="imgStyle" />
+  <img :src="state.imgSrc" :style="imgStyle" />
   <hr />
   <button @click="changeImage">Cambiar imagen</button>
-  <input type="color" v-model="borderColor" />
+  <input type="color" v-model="state.borderColor" />
   <hr />
   <label for="grayscaleFilter">Escala de gris</label>
-  <input type="range" v-model="grayscaleFilter" min="0" max="100" step="1" />
+  <input type="range" v-model="state.grayscaleFilter" min="0" max="100" step="1" />
   <hr />
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 
-const borderColor = ref("#FF0000");
-const grayscaleFilter = ref(0);
-const imgSrc = ref("https://source.unsplash.com/400x400/?cocktail,party&v=1");
+const state = reactive({
+    borderColor: "#FF0000",
+    grayscaleFilter: 0,
+    imgSrc: "https://source.unsplash.com/400x400/?cocktail,party&v=1"
+})
 
 const changeImage = () => {
-  imgSrc.value = `https://source.unsplash.com/400x400/?cocktail,party&v=${Math.floor(
+  state.imgSrc = `https://source.unsplash.com/400x400/?cocktail,party&v=${Math.floor(
     Math.random() * 300
   )}`;
 };
@@ -26,8 +28,8 @@ const imgStyle = computed(() => {
   return {
     "border-width": "20px",
     borderStyle: "solid",
-    borderColor: borderColor.value,
-    filter: `grayscale(${grayscaleFilter.value}%)`,
+    borderColor: state.borderColor,
+    filter: `grayscale(${state.grayscaleFilter}%)`,
   };
 });
 
